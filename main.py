2606f012ca04d8a4f41d338f5dea8901f38f1243
tynan <tynandavis@gmail.com>
2021-10-26 01:16:52 +0100

    began the project. Figured out how to create the application loop with the gui window and update it with play, pause and exit functionality. todo next is to add details to the gui window so it can represent a simple 2d map

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+#imports
+import PySimpleGUI as sg
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+#main code
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    closeBlayout = [[sg.Button('Play'), sg.Button('Pause'), sg.Button('Close')]]
+    mainLayout = [[sg.Text('app is waiting to run', key='_text_')]]
+    layout = [
+        [sg.Column(mainLayout, size=(400, 400), key='mainWindow')],
+        [sg.Column(closeBlayout, justification='right')]
+    ]
+    window = sg.Window("app", layout)
+    frame = 0
+    play = False
+    #program run loop
+    while True:
+        event, values = window.read(timeout=100)
+        if event == 'Play':
+            play = True
+        if event == 'Pause':
+            play = False
+        if play:
+            frame += 1
+            window['_text_'].update('app is running {}'.format(frame))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        #exit run loop event
+        if event == "Close" or event == sg.WIN_CLOSED:
+            break
+
+        window.refresh()
+
+    window.close()
