e208a32e4f9fc0953833faf1f7cf4ffb9416bd51
tynan <tynandavis@gmail.com>
2021-11-07 17:46:13 +0000

    SLOOOOWLY COMING TOGETHER THE UI DO BUILD COMMANDS NEXT THEN DONE?!

--- grid_controller.py
+++ grid_controller.py
@@ -22,17 +22,30 @@ def create_flat_grid(x=v.max_x, y=v.max_y):
         print('max dimensions exceeded')
         exit()
     a = np.empty(x)
-    a.fill(v.max_z/2)
+    a.fill(v.max_z/3)
     grid = []
     for i in range(0,y):
         grid.append(a)
     return np.array(grid)
 
+def check_depth(grid, r_x, r_y):
+    if v.max_z >= grid[r_x][r_y] >= 0:
+        return True
+    return False
+
+def dig(grid, r_x, r_y):
+    if check_depth(grid, r_x, r_y):
+        grid[r_x][r_y] = grid[r_x][r_y] - 1
+    return grid
+
 def create_river(grid, r_x, r_y):
-    grid[r_x][r_y] = grid[r_x][r_y] - 2
+    if check_depth(grid, r_x, r_y):
+        grid[r_x][r_y] = grid[r_x][r_y] - 1
     for i in range(-1, 2):
         for j in range(-1, 2):
-            if (len(grid[0]) > r_x - i > -1) and (len(grid) > r_y - j > -1):
+            test_x = r_x - i
+            test_y = r_y - j
+            if (len(grid[0]) > test_x > -1) and (len(grid) > test_y > -1) and check_depth(grid, test_x, test_y):
                 print (r_x - i, ', ', r_y - j)
                 grid[r_x - i][r_y - j] = grid[r_x - i][r_y - j] - 1
     return grid
--- simplegui.py
+++ simplegui.py
@@ -15,38 +15,62 @@ def draw_figure(canvas, figure):
 
 def runUI():
     #creating layout
-    button_layout = [[Sg.Button('Show Map'), Sg.Button('Pause'), Sg.Button('Close')]]
-    mainLayout = [[Sg.B(' ', size=(2, 1), key=(i, j)) for i in range(v.max_x)] for j in range(v.max_y)]
+    button_layout = [[Sg.Text('Current Selected: (0,0)',key='selectedT'), Sg.Text('Current Mode: select', key='modeT'), Sg.Button('Create River'), Sg.Button('Dig'), Sg.Button('Select Mode'), Sg.Button('Save Map'), Sg.Button('Reset Map'), Sg.Button('Close')]]
+    mainLayout = [[Sg.B(' ', size=(2, 1), key=(i, j), button_color='gray') for i in range(v.max_x)] for j in range(v.max_y)]
     layout = [
         [Sg.Column(mainLayout, size=(400, 350), key='gridWindow'),Sg.Canvas(key='figCanvas',size=(200, 100))],
         [Sg.Column(button_layout, justification='right')]
     ]
     window = Sg.Window("app", layout)
-    frame = 0
-    play = False
+
     grid = gc.create_flat_grid()
     figAgg = False
-    # program run loop
+    selected = [0, 0]
+
+    mode = 'select'
     while True:
-        event, values = window.read(timeout=100)
-        #print (event)
-        if event == 'Show Map':
-            if figAgg != False:
-                figAgg.get_tk_widget().forget()
-            fig, ax = outD.showTableState(grid)
-            figAgg = draw_figure(window['figCanvas'].TKCanvas, fig)
-            play = True
+        event, values = window.read(timeout=10000)
+
+        if event == 'Save Map':
+            print ('save map')
+
         for i in range(v.max_x):
             for j in range(v.max_y):
                 if event == (i, j):
+                    window[selected[0], selected[1]].update(button_color='gray')
                     print ('pressed: ', i, ', ', j)
-                    grid = gc.create_river(grid, i, j)
-        if event == 'Pause':
-            play = False
-        if play:
-            frame += 1
-            #window['_text_'].update('app is running {}'.format(frame))
+                    selected = [i, j]
+                    window[event].update(button_color='red')
+                    window['selectedT'].update('current selected: {}'.format(selected))
 
+
+        if mode == 'river':
+            for i in range(v.max_x):
+                for j in range(v.max_y):
+                    if event == (i, j):
+                        print('pressed: ', i, ', ', j)
+                        selected = [i, j]
+                        window['selectedT'].update('current selected: {}'.format(selected))
+                        grid = gc.create_river(grid, selected[0], selected[1])
+
+        if event == 'Dig':
+            grid = gc.dig(grid, selected[0], selected[1])
+
+        if event == 'Create River':
+            mode = 'river'
+            window['modeT'].update(mode)
+
+        if event == 'Reset Map':
+            grid = gc.create_flat_grid()
+
+        if event == 'Select Mode':
+            mode = 'select'
+            window['modeT'].update(mode)
+
+        if figAgg != False:
+            figAgg.get_tk_widget().forget()
+        fig, ax = outD.showTableState(grid)
+        figAgg = draw_figure(window['figCanvas'].TKCanvas, fig)
         # exit run loop event
         if event == "Close" or event == Sg.WIN_CLOSED:
             break
